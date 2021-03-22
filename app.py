5405d7fc3d3b87de9f08055b2ec53dc9a0e78159
Yuvraj Dagur <yuvraj.dagur928@gmail.com>
2021-03-22 21:17:45 +0530

    connect MySQL Database

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, request, jsonify, render_template, session, redirect, flash
+from flask_mysqldb import MySQL
+from os import urandom
+from yaml import load, FullLoader
 
 app = Flask(__name__)
+mysql = MySQL(app)
+
+# MySQL Configuration
+db_keeps = load(open('data.yaml'), Loader=FullLoader)
+app.config['MYSQL_HOST'] = db_keeps['mysql_host']
+app.config['MYSQL_USER'] = db_keeps['mysql_user']
+app.config['MYSQL_PASSWORD'] = db_keeps['mysql_password']
+app.config['MYSQL_DB'] = db_keeps['mysql_db']
+app.config['SECRET_KEY'] = urandom(24)
 
 @app.route('/')
 def index():
