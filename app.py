d38ca30c00326a79b1bbafdb66e4b536c4a06cb7
Yuvraj Dagur <yuvraj.dagur928@gmail.com>
2021-03-22 14:58:35 +0530

    Google SignIn initiated

--- app.py
+++ app.py
@@ -6,5 +6,50 @@ app = Flask(__name__)
 def index():
     return 'This is the beginning of a revolution'
 
+@app.route('/register/')
+def register():
+    pass
+
+@app.route('/login/')
+def login():
+    return render_template('login.html')
+
+@app.route('/banking/')
+def banking():
+    pass
+
+@app.route('/farming/')
+def farming():
+    pass
+
+@app.route('/healthcare/')
+def healthcare():
+    pass
+
+@app.route('/govt-schemes/')
+def schemes():
+    pass
+
+@app.route('/miscellaneous/')
+def misc():
+    pass
+
+@app.route('/blogs/')
+def blogs():
+    pass
+
+@app.route('/issues/new/')
+def new_issue():
+    pass
+
+@app.route('/blogs/new/')
+def new_blog():
+    pass
+
+@app.route('/profile/me/')
+def me():
+    pass
+
+
 if __name__ == '__main__':
     app.run(debug=True)
