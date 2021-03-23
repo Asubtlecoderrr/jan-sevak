cebc9ad345da4ad292562b3459512fd074567faa
Yuvraj Dagur <yuvraj.dagur928@gmail.com>
2021-03-23 16:45:45 +0530

    level

--- app.py
+++ app.py
@@ -17,10 +17,31 @@ app.config['SECRET_KEY'] = urandom(24)
 
 @app.route('/')
 def index():
-    return 'This is the beginning of a revolution'
+    if session['login'] == True:
+        return redirect('/home/')
+    return render_template('home.html')
 
-@app.route('/register/', methods=['GET', 'POST'])
-def register():
+@app.route('/home/')
+def home():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM issue;")
+    q = cur.fetchall()
+    if q > 0:
+        issues = cur.fetchall()
+        return render_template('index.html', issues=issues)
+    else:
+        issues = None
+        return render_template('index.html', issues=issues)
+    return render_template('index.html')
+
+@app.route('/user/register/', methods=['GET', 'POST'])
+def reg_user():
+    if request.method == 'POST':
+        pass
+    return render_template('register.html')
+
+@app.route('/professional/register/', methods=['GET', 'POST'])
+def reg_prof():
     if request.method == 'POST':
         pass
     return render_template('register.html')
@@ -34,33 +55,35 @@ def continue_reg():
 def login():
     return render_template('login.html')
 
-@app.route('/banking/')
-def banking():
-    pass
+@app.route('/domain/<domain>')
+def domain(domain):
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM issues WHERE domain='{}';".format(domain))
+    domain = cur.fetchall()
 
-@app.route('/farming/')
-def farming():
-    pass
 
-@app.route('/healthcare/')
-def healthcare():
-    pass
-
-@app.route('/govt-schemes/')
-def schemes():
-    pass
-
-@app.route('/miscellaneous/')
-def misc():
-    pass
-
-@app.route('/blogs/')
-def blogs():
-    pass
-
-@app.route('/issues/new/')
+@app.route('/issues/new/', methods=['GET', 'POST'])
 def new_issue():
-    pass
+    if request.method == 'POST':
+        form = request.form
+        domain = form['domain']
+        complaint = form['complaint']
+        link = form['link']
+        name = session['fullname']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO issue(domain, complaint, link, name) VALUES(%s, %s, %s, %s);", (domain, complaint, link, name))
+        mysql.connection.commit()
+        cur.close()
+        flash("Issue Posted Successfully", "success")
+        return redirect('/home/')
+    return render_template('new_issue.html')
+
+@app.route('/issues/<int:id>/')
+def issue_id(id):
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM issue WHERE id={}".format(id))
+    issue = cur.fetchone()
+
 
 @app.route('/blogs/new/')
 def new_blog():
