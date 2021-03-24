ce0ca4516510daabaaaef2a9f629f76e3b60a1e3
Yuvraj Dagur <yuvraj.dagur928@gmail.com>
2021-03-24 05:50:07 +0530

    major push

--- app.py
+++ app.py
@@ -71,6 +71,7 @@ def login_user():
                 session['logged_in'] = True
                 session['full_name'] = user[1]
                 session['district'] = user[3]
+                session['id'] = user[0]
                 flash(f"Welcome {session['full_name']}!! Your Login is Successful", 'success')
             else:
                 cur.close()
@@ -88,12 +89,12 @@ def login_user():
 def login_prof():
     return render_template('login.html', role='professional')
 
-@app.route('/domain/<domain>')
+@app.route('/domain/<domain>/')
 def domain(domain):
     cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM issues WHERE domain='{}';".format(domain))
-    domain = cur.fetchall()
-    return render_template('domain.html', domain=domain)
+    cur.execute("SELECT * FROM issue WHERE domain='{}';".format(domain))
+    issues = cur.fetchall()
+    return render_template('issue.html', issues=issues, domain=domain)
 
 @app.route('/issues/new/', methods=['GET', 'POST'])
 def new_issue():
@@ -102,30 +103,43 @@ def new_issue():
         domain = form['domain']
         complaint = form['complaint']
         link = form['link']
-        name = session['fullname']
+        name = session['full_name']
         cur = mysql.connection.cursor()
         cur.execute("INSERT INTO issue(domain, complaint, link, name) VALUES(%s, %s, %s, %s);", (domain, complaint, link, name))
         mysql.connection.commit()
         cur.close()
         flash("Issue Posted Successfully", "success")
         return redirect('/home/')
-    return render_template('new_issue.html')
+    return render_template('new.html')
 
 @app.route('/issues/<int:id>/')
 def issue_id(id):
     cur = mysql.connection.cursor()
     cur.execute("SELECT * FROM issue WHERE id={}".format(id))
     issue = cur.fetchone()
+    return render_template('issue.html', issue=issue)
 
+@app.route('/blogs/')
+def blog():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM blog;")
+    blogs = cur.fetchall()
+    return render_template('blog.html', blogs=blogs)
 
-@app.route('/blogs/new/')
-def new_blog():
-    pass
+@app.route('/blogs/<int:id>/')
+def blog_id(id):
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM issue WHERE id={}".format(id))
+    issue = cur.fetchone()
 
-@app.route('/profile/me/')
+@app.route('/my-issues/')
 def me():
-    pass
-
+    cur = mysql.connection.cursor()
+    q = cur.execute("SELECT * FROM issue WHERE name='{}';".format(session['full_name']))
+    if q > 0:
+        my_issues = cur.fetchall()
+        return render_template('myissues.html', issues=my_issues)
+    return render_template('myissues.html', issues=None)
 
 if __name__ == '__main__':
     app.run(debug=True)
